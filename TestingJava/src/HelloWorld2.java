2a968ec75d683465b3c6cbefc38e6d7e71694314
Moomal Abbasi <Suhni Abbasi@DESKTOP-94BGEC4>
2020-02-09 22:32:18 +0500

    code deleted

--- TestingJava/src/HelloWorld2.java
+++ TestingJava/src/HelloWorld2.java
@@ -3,8 +3,7 @@ public class HelloWorld2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-		System.out.println("Hello by me three");
+		
 
 
 	}
